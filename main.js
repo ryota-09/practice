f8adfaf34c59018d9e86d00161e4bc3bc79ac472
ryota-09 <n.ryo950911@gmail.com>
2021-10-05 10:45:13 +0900

    dom-last

--- main.js
+++ main.js
@@ -1,13 +1,49 @@
+(()=> {
+let todos = [];
 
-const getMain = document.getElementById('main');
-const createOl = document.createElement('ol');
+const todoElement = document.getElementById('todo');
+const taskButtonElement = document.getElementById('taskButton');
+const listElement = document.getElementById('list');
 
-getMain.appendChild(createOl);
-
-
-for (let i = 1; i < 6; i++ ){
-  const createLi = document.createElement('li');
-  createLi.textContent = `アイテム${i}`;
+taskButtonElement.addEventListener('click', (event)=>{
   
-  createOl.parentNode.appendChild(createLi);
+  const todo = todoElement.value;
+  todoElement.value = '';
+
+
+  if (todo) {
+    todos.push(todo);
+    showTodos();
+  }
+})
+
+const showTodos = () => {
+
+  while(listElement.firstChild){
+    listElement.removeChild(listElement.firstChild);
+  }
+
+  todos.forEach((todo, index) => {
+    const todoItem = document.createElement('li');
+    const taskNumber = index + 1;
+
+    todoItem.textContent = `${taskNumber}:${todo}`;
+    listElement.appendChild(todoItem);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '削除';
+    todoItem.appendChild(deleteButton);
+    
+    deleteButton.addEventListener('click', (event) => {
+      deleteTodos(index);
+    });
+  })
 }
+
+const deleteTodos = (index) => {
+  todos.splice(index, 1);
+  showTodos();
+}
+  
+
+})();
